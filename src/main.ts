55e8bd678dbcc0695c97040aaaeec5bb2d81154f
Azizxon <muzaffarovazizkhon@gmail.com>
2023-03-24 16:48:25 +0500

    Built fundament

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from "cookie-parser"
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const start = async () => {
+  try {
+    const PORT = process.env.PORT
+    const app = await NestFactory.create(AppModule)
+    app.setGlobalPrefix("api")
+    app.use(cookieParser())
+    await app.listen(PORT, () => {
+      console.log(`Server ${PORT}-portda`);
+    })
+  } catch (error) {
+    console.log(error);
+    
+  }
 }
-bootstrap();
+
+start()
